f5813c6b987792389e99fb52b49d50ab14bd7839
Erik Jälevik <erik.jalevik@futurice.com>
2016-12-24 13:00:08 +0100

    Improve comment

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,7 @@ import { createStore, combineReducers } from 'redux'
 import collectionReducer from './collectionReducer'
 import testReducer from './testReducer'
 
-// This statement replaces the initial state. The state for each piece of data
-// is initialised by its reducer.
+// This creates a rootReducer function. The initial state for data subtree is initialised by its reducer.
 const rootReducer = combineReducers({
   collections: collectionReducer,
   testCounter: testReducer
