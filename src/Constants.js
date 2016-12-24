4a921ac8e5556ab4a2450909475f7a07e3d11d95
Erik Jälevik <erik.jalevik@futurice.com>
2016-12-24 12:14:51 +0100

    Case-sensitive rename step 1

--- src/Constants.js
+++
@@ -1,9 +0,0 @@
-export default Constants = {
-  backgroundColor: "hsl(220, 100%, 8%)",
-  textColor: "hsl(190, 100%, 60%)",
-  textColorDisabled: "hsl(190, 20%, 35%)",
-
-  bodyFontSize: 22,
-  inputFontSize: 18,
-  buttonFontSize: 18
-}
